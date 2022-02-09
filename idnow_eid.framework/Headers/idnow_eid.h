63ef6cb2546c4859f4312e829a5bedc0b6ebbd89
Aare Undo <aare.undo@gmail.com>
2022-02-09 17:35:39 +0200

    added eid_framework

---
+++ idnow_eid.framework/Headers/idnow_eid.h
@@ -0,0 +1,20 @@
+//
+//  idnow_eid.h
+//  idnow_eid
+//
+//  Created by Phu Nguyen on 2/7/20.
+//  Copyright © 2020 IDnow Gmbh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for idnow_eid.
+FOUNDATION_EXPORT double idnow_eidVersionNumber;
+
+//! Project version string for idnow_eid.
+FOUNDATION_EXPORT const unsigned char idnow_eidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <idnow_eid/PublicHeader.h>
+
+
